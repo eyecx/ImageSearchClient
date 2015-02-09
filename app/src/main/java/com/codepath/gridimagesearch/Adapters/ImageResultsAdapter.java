14fb0edce9c0ca0ea665c75bf9e45da8da8ebb2d
Edmund Ye <edmund.ye@airbnb.com>
2015-02-09 15:02:52 -0800

    Use ViewHolder patter

--- app/src/main/java/com/codepath/gridimagesearch/Adapters/ImageResultsAdapter.java
+++ app/src/main/java/com/codepath/gridimagesearch/Adapters/ImageResultsAdapter.java
@@ -19,20 +19,30 @@ import java.util.List;
  * Created by edmundye on 2/8/15.
  */
 public class ImageResultsAdapter extends ArrayAdapter<ImageResult> {
+
+    private static class ViewHolder {
+        ImageView ivImage;
+        TextView tvTitle;
+    }
     public ImageResultsAdapter (Context context, List <ImageResult> images ){
         super(context, R.layout.item_image_result, images);
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageResult imageInfo = getItem(position);
+        ViewHolder viewHolder;
         if (convertView == null) {
+            viewHolder = new ViewHolder();
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_image_result, parent, false);
+            viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
+            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
-        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
-        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-        ivImage.setImageResource(0);
-        tvTitle.setText(Html.fromHtml(imageInfo.title));
-        Picasso.with(getContext()).load(imageInfo.thumbUrl).into(ivImage);
+        viewHolder.ivImage.setImageResource(0);
+        viewHolder.tvTitle.setText(Html.fromHtml(imageInfo.title));
+        Picasso.with(getContext()).load(imageInfo.thumbUrl).into(viewHolder.ivImage);
         return convertView;
     }
 }
